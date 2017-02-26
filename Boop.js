c8418d19092861fb7bbeb761e1de6dd4582f9599
Vincent Liok <vliok@stuy.edu>
2017-02-26 12:53:33 -0500

    finished

--- Boop.js
+++ Boop.js
@@ -1,13 +1,18 @@
 var canv = document.getElementById("canv");
 var ctx = canv.getContext("2d");
-canv.addEventListener("click", circle);
 
-var b = document.getElementById("b");
-b.addEventListener("click",stopIt);
+var stop = document.getElementById("stop");
+stop.addEventListener("click",stopIt);
+
+var growing = document.getElementById("growing");
+growing.addEventListener("click",growCircle);
+
+var DVD = document.getElementById("DVD");
+DVD.addEventListener("click",move);
 
 var rid = 0;
 
-function start() {
+function growCircle() {
     var x = 0;
     var grow = true;
     
@@ -16,32 +21,62 @@ function start() {
 	
 	function expandCircle() {
 	    ctx.clearRect(0,0,canv.width,canv.height);
-	    //console.log("boop");
-	    //console.log(rid);
 	    ctx.beginPath();
 	    ctx.arc(canv.width/2, canv.height/2, x, 0, 2 * Math.PI);
 	    ctx.fill();
-	    x++;
-	    rid = window.requestAnimationFrame( createCircle );
+	    x = x + 3;
+	    if (x >= canv.width/2 || x >= canv.height/2) {
+                grow = false;
+            };
 	};
 	
 	function shrinkCircle() {
 	    ctx.clearRect(0,0,canv.width,canv.height);
-	    //console.log("boop");
-	    //console.log(rid);
 	    ctx.beginPath();
 	    ctx.arc(canv.width/2, canv.height/2, x, 0, 2 * Math.PI);
 	    ctx.fill();
-	    x--;
-	    rid = window.requestAnimationFrame( createCircle );
+	    x = x - 3;
+	    if (x == 0) {
+                grow = true;
+            };
 	};
 
-	if (x >= canv.width/2) || (x >= canv.height/2) {
-	    grow = false;
+	if (grow == true) {
+	    expandCircle();
+	}
+	else {
+	    shrinkCircle();
 	};
-
+	rid = window.requestAnimationFrame( circle );
+	
     };
     
+    circle();
+    
+};
+
+function move() {
+    window.cancelAnimationFrame( rid );
+    var x = canv.width/2;
+    var y = canv.height/2;
+    var xspeed = 3;
+    var yspeed = 3;
+    function moveCircle() {
+	if (x >= canv.width - 50 || x <= 50) {
+	    xspeed = -xspeed;
+	};
+	if (y >= canv.height - 50 || y <= 50) {
+	    yspeed = -yspeed;
+	};
+	ctx.clearRect(0,0,canv.width,canv.height);
+	ctx.beginPath();
+	ctx.arc(x, y, 50, 0, 2 * Math.PI);
+	ctx.fill();
+	x = x + xspeed;
+	y = y + yspeed;
+	rid = window.requestAnimationFrame( moveCircle );
+    };
+    moveCircle();
 };
 
 function stopIt() {
